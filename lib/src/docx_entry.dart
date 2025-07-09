ed340515aa9ef8374e6ea38c4d420912ae3c2a22
Ian <79951156+joseianpatrick@users.noreply.github.com>
2025-07-09 19:39:21 +0800

    Revert "Update docx_entry.dart"
    This reverts commit a1ed0673bc3bc90d9b8e35ea0f7346a441533524.

--- lib/src/docx_entry.dart
+++ lib/src/docx_entry.dart
@@ -26,10 +26,7 @@ abstract class DocxEntry {
     if (_index < 0) {
       arch.addFile(ArchiveFile(_name, data.length, data));
     } else {
-      final f = List<ArchiveFile>.from(arch.files);
-      f[_index] = ArchiveFile(_name, data.length, data);
-      arch.files.clear();
-      arch.files.addAll(f);
+      arch.files[_index] = ArchiveFile(_name, data.length, data);
       // arch.modifyAtIndex(_index, ArchiveFile(_name, data.length, data));
     }
   }
